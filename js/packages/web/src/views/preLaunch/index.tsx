79c17439e94ade7f1a0f225e897637031616b950
Oscar Alberto Salamanca Chacón <77991664+thanathos9558@users.noreply.github.com>
2021-12-01 09:35:33 -0500

    Update index.tsx

--- js/packages/web/src/views/preLaunch/index.tsx
+++ js/packages/web/src/views/preLaunch/index.tsx
@@ -23,7 +23,7 @@ const DiscordButton = () => (
   <a
     className={'discord-button'}
     target={'_blank'}
-    href={'https://discord.com/invite/metaplex'}
+    href={'https://discord.gg/abQQGvUye2'}
   >
     <span></span> Join our Discord
   </a>
